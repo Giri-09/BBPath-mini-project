ecabe58b929273ddd6b5a9a889a80123629c86ea
Giri-09 <nsgiri4444@gmail.com>
2023-05-05 16:53:18 +0530

    may05-giri

---
+++ my-app/src/components/HomeCard/index.js
@@ -0,0 +1,13 @@
+import "./index.css"
+const HomeCard=()=>(
+    <div className="card custom-card ">
+    <img src="https://pvbooks.in/wp-content/uploads/2020/06/IMG-20200605-WA0006-1.jpg" className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+)
+
+export default HomeCard
