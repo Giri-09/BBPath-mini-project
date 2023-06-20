dbab9808416a42995cce03321e559fab66f2e300
Giri-09 <nsgiri4444@gmail.com>
2023-06-20 23:30:11 +0530

    Added cards

--- my-app/src/components/CSECoursesCard/index.js
+++ my-app/src/components/CSECoursesCard/index.js
@@ -1,34 +1,73 @@
-import CustomNavbar  from '../Navbar';
-import Footer  from '../Footer';
-import { Fragment } from 'react';
+import CustomNavbar from "../Navbar";
+import Footer from "../Footer";
+import { Fragment } from "react";
 
-import "./index.css"
+import "./index.css";
 
-const CSECoursesCard=()=>(
-    <Fragment>
-      <CustomNavbar />
-      <div className='cse-courses-bg d-flex flex-column justify-content-center align-items-center'>
-         <div className="row" >
-  <div className="col-sm-6">
-    <div className="card cse-course-card">
-    <img src="https://prepbytes-misc-images.s3.ap-south-1.amazonaws.com/assets/1644827956714-6.%20Programming%20Languages.png" className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Programming</h5>
-        <p className="card-text">Learn programming from basics in this online training.Programming course is taught hands-on by experts.Best for beginners.Start now!</p>
-        <a href="#" className="btn btn-primary">Get Started</a>
+export const courseList = [
+  {
+    cardTitle: "Programming",
+    cardText:
+      "Learn programming from basics in this online training.Programming course is taught hands-on by experts.Best for beginners.Start now!",
+    image: "programming.png",
+  },
+  {
+    cardTitle: "DataStructures and Algorithms",
+    cardText:
+      "Learn different DataStructures and Algorithms used to program.In this session we'll be understanding the concept of DataStructures and implementation of algorithms along with their analysis based on time and space complexity",
+    image: "dsa.png",
+  },
+  {
+    cardTitle: "Database Management System",
+    cardText:
+      "Learn database management system from basics in this online training.DBMS course taught hands-on by experts.Learn key concepts in relational database management system in detail with examples",
+    image: "dbms.png",
+  },
+  {
+    cardTitle: "Front-End Development",
+    cardText:
+      "With this online course you can understand how websites work and how HTML,CSS and JavaScript contribute.Learn fundamentals of implementing responsive web design,How to use bootstrap.",
+    image: "frontend.png",
+  },
+  {
+    cardTitle: "Back-End Development",
+    cardText:
+      "Hi and welcome to Complete backend web developer with nodeJS with projects. A complete course that starts with Javascript basics and move towards advance part of javascript. After that we will move to nodejs part. ",
+    image: "backend.png",
+  },
+  {
+    cardTitle: "Software-Testing",
+    cardText:
+      "Course objective is to provide you with all required knowledge that you need in order to land your first software testing 	job whether it is a full-time or a freelancing job.",
+    image: "st.png",
+  },
+];
+
+const CSECoursesCard = () => (
+  <Fragment>
+    <CustomNavbar />
+    <div className="cse-courses-bg d-flex flex-column justify-content-center align-items-center">
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="card">
+            {courseList.map((each,index) => (
+              <div className="card-body" key={index}>
+                {/* <img src={`images/cse-courses-card-images/${each.image}`} /> */}
+
+                <h5 className="cardTitle">{each.cardTitle}</h5>
+                <p className="cardText">{each.cardText}</p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
-  </div>
 
-      </div>
-    </div>
-  
+    <Footer />
+  </Fragment>
+);
 
-   
-<Footer />
-</Fragment>
-
-   
-)
-
-export default CSECoursesCard
+export default CSECoursesCard;
--- my-app/src/components/HomeCard/index.js
+++ my-app/src/components/HomeCard/index.js
@@ -1,14 +1,27 @@
 import "./index.css"
+
+const homeCardsList=[
+    {cardTitle:"Number 1",
+cardText:"hello hello",
+imageUrl:"nameOfImage"},
+{cardTitle:"Number 1",
+cardText:"hello hello",
+imageUrl:"nameOfImage"},
+]
+
 const HomeCard=()=>(
     <div className="card custom-card mb-3">
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLFg8KkVkt7DDILJTVA7Y195YclMprz-6qog&usqp=CAU" className="card-img-top" alt="..." />
-    <div className="card-body">
-      <h5 className="card-title">COMPUTER SCIENCE COURSES</h5>
-      <p className="card-text">Our BBPath COMPUTER SCIENCE courses will train you and help you to land on your dream job</p>
-<a href="" className="btn btn-primary">Get Started</a>
+    {homeCardsList.map((each,index)=>(
+        <div className="card-body" key={index}>
+        <h5 className="card-title">{each.cardTitle}</h5>
+        <p className="card-text">{each.cardText}</p>
+  <a href="" className="btn btn-primary">Get Started</a>
+      </div>
+    )
+    )}
     </div>
-    </div>
-git status
+
 
    
 )
