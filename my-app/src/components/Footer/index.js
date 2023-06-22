ab4d7f235c9a226d876023c77a38efa0b939005a
Golla Sairam <gollasairam918@gmail.com>
2023-06-22 11:28:46 +0530

    footer content

--- my-app/src/components/Footer/index.js
+++ my-app/src/components/Footer/index.js
@@ -3,8 +3,8 @@ const Footer=()=>(
   <div className="container">
     <div className="row">
       <div className="col-md-6">
-        <h3>Company Name</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit dolor eget magna lobortis, id dignissim velit feugiat. Nunc eu mi lorem.</p>
+        <h3>BALAJI'S BRIGTH PATH</h3>
+        <p>Lets Learn and Grow with us</p>
       </div>
       <div className="col-md-3">
         <h4>Links</h4>
@@ -18,10 +18,10 @@ const Footer=()=>(
       <div className="col-md-3">
         <h4>Contact Us</h4>
         <ul className="list-unstyled">
-          <li>123 Main St.</li>
-          <li>City, State ZIP</li>
-          <li>(123) 456-7890</li>
-          <li>email@example.com</li>
+          <li>Nandikotkur.</li>
+          <li>Kurnool, Andhra Pradesh 518401</li>
+          <li>(+91) 9494595901</li>
+          <li>balajisbrightpath@gmail.com</li>
         </ul>
       </div>
     </div>
